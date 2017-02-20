c86f077d6f7a262c03dbfd9cd29b8ef79bc68f46
Tejas Kumar <tejas.kumar@mcmakler.de>
2017-02-20 18:58:24 +0100

    Boilerplate, initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,26 @@
-const webpack = require('webpack');
+// const webpack = require('webpack');
+const DashboardPlugin = require('webpack-dashboard/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-	entry: './index.js',
-	output: {
-		file: './bundle.js'
-	},
+  entry: './src/app.jsx',
+  output: {
+    filename: './bundle.js',
+    path: path.resolve(__dirname),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `${path.resolve(__dirname)}/src/index.html`,
+    }),
+    new DashboardPlugin(),
+  ],
 };
