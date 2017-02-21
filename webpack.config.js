fc9e954bb1cf7efe174755892227a283d6e7600c
Tejas Kumar <tejas.kumar@mcmakler.de>
2017-02-21 16:07:18 +0100

    2 lines

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,46 @@
-// const webpack = require('webpack');
+const webpack = require('webpack');
 const DashboardPlugin = require('webpack-dashboard/plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/app.jsx',
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './app.jsx',
+  ],
+  context: path.resolve(__dirname, 'src'),
   output: {
     filename: './bundle.js',
     path: path.resolve(__dirname),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    hot: true,
+    publicPath: '/',
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        loader: 'babel-loader',
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'postcss-loader', 'css-loader?modules', 'sass-loader'],
       },
     ],
+  },
+  resolve: {
+    extensions: ['js', 'jsx'],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: `${path.resolve(__dirname)}/src/index.html`,
     }),
     new DashboardPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
   ],
 };
