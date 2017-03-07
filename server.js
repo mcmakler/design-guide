a96a229a21af4518f9b6bbd5409ccc0e2a867f9d
Tejas Kumar <tejas.kumar@mcmakler.de>
2017-03-07 18:04:54 +0100

    Added SASS Lint

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const server = require('pushstate-server');
 
 const port = process.env.PORT || 9001;
 
-console.log(`!! [MCMAKLER-STYLEGUIDE]: Listening on port ${port} !!`);
-
 server.start({
   port,
   directory: './dist',
