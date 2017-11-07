724b0674ddc33073a6976357dbbe4ee1e8c2eefa
Kathrin Holzmann <kathrin.holzmann@mcmakler.de>
2017-11-07 17:55:49 +0100

    refactor example page

--- src/components/App/FormPage/FormPage.component.jsx
+++ src/components/App/FormPage/FormPage.component.jsx
@@ -2,31 +2,11 @@ import React from 'react';
 
 import Page from '../Page/Page.component';
 import PreviewBox from '../PreviewBox/PreviewBox.component';
-import StyledSelect from '../StyledSelect/StyledSelect.component';
 import CodeSnippet from '../CodeSnippet/CodeSnippet.component';
 
-import { cssInput, htmlRadio } from './snippets/form.snippet';
+import { cssInput, htmlRadio, cssRadio, htmlSelect, cssSelect } from './snippets/form.snippet';
 
 import './FormPage.scss';
-
-const selectOptions = [
-  {
-    id: 1,
-    option: 'Berlin',
-  },
-  {
-    id: 2,
-    option: 'Mieten',
-  },
-  {
-    id: 3,
-    option: 'Preis bis 22193€',
-  },
-  {
-    id: 4,
-    option: 'ab 2 Zimmer',
-  },
-];
 
 export default () => (
 
@@ -35,81 +15,124 @@ export default () => (
     className="form-page"
     subtitle="This is how form shall look like"
   >
-    <div className="container page-section" >
-      <div className="columns">
-        <div className="column">
-          <CodeSnippet language="css">
-            { cssInput }
-          </CodeSnippet>
-        </div>
-        <div className="column">
-          <PreviewBox className="center">
-            <div className="FormPage__form">
-              <input type="text" placeholder="Mein Vorname" />
-
-              <textarea placeholder="Meine Nachricht" />
-            </div>
-          </PreviewBox>
+    <div className="container">
+      <div className="page-section">
+        <header>
+          <h2 className="heading heading_secondary">Text input</h2>
+        </header>
+        <div className="columns">
+          <div className="column">
+            <CodeSnippet language="css">
+              { cssInput }
+            </CodeSnippet>
+          </div>
+          <div className="column">
+            <PreviewBox className="center">
+              <div className="FormPage__form">
+                <input type="text" placeholder="Mein Vorname" />
+                <textarea placeholder="Meine Nachricht" />
+              </div>
+            </PreviewBox>
+          </div>
         </div>
       </div>
       <hr />
-      <div className="columns">
-        <div className="column">
-          <CodeSnippet language="html">
-            { htmlRadio }
-          </CodeSnippet>
+      <div className="page-section">
+        <header>
+          <h2 className="heading heading_secondary">Radio Buttons</h2>
+        </header>
+        <div className="columns">
+          <div className="column">
+            <CodeSnippet language="html">
+              { htmlRadio }
+            </CodeSnippet>
+          </div>
+          <div className="column">
+            <CodeSnippet language="css">
+              { cssRadio }
+            </CodeSnippet>
+          </div>
         </div>
-        <div className="column">
-          <PreviewBox className="center">
-            <div className="FormPage__form">
-              <div className="radio--styled">
-                <input
-                  type="radio"
-                  id="radio_gender_0"
-                  name="radio_gender_[gender]"
-                  required="required"
-                  value="0"
-                  checked="checked"
-                />
-                <label className="radio__label--styled" htmlFor="radio_gender_0">
-                  Frau
-                </label>
+        <div className="columns">
+          <div className="column">
+            <PreviewBox className="center">
+              <div className="FormPage__form">
+                <div className="radio--styled">
+                  <input
+                    type="radio"
+                    id="radio_gender_0"
+                    name="radio_gender_[gender]"
+                    required="required"
+                    value="0"
+                    checked="checked"
+                  />
+                  <label className="radio__label--styled" htmlFor="radio_gender_0">
+                    Frau
+                  </label>
+                </div>
+                <div className="radio--styled">
+                  <input
+                    type="radio"
+                    id="radio_gender_1"
+                    name="radio_gender_[gender]"
+                    required="required"
+                    value="1"
+                  />
+                  <label htmlFor="radio_gender_1" className="radio__label--styled">
+                    Herr
+                  </label>
+                </div>
               </div>
-              <div className="radio--styled">
-                <input
-                  type="radio"
-                  id="radio_gender_1"
-                  name="radio_gender_[gender]"
-                  required="required"
-                  value="1"
-                />
-                <label htmlFor="radio_gender_1" className="radio__label--styled">
-                  Herr
-                </label>
-              </div>
-            </div>
-          </PreviewBox>
+            </PreviewBox>
+          </div>
         </div>
       </div>
       <hr />
-      <div className="columns">
-        <div className="column">
-          <PreviewBox>
-            <div>
-              <StyledSelect
-                options={selectOptions}
-                kind="primary"
-
-              />
-            </div>
-            <div>
-              <StyledSelect
-                options={selectOptions}
-                kind="secondary"
-              />
-            </div>
-          </PreviewBox>
+      <div className="page-section">
+        <header>
+          <h2 className="heading heading_secondary">Selects</h2>
+        </header>
+        <div className="columns">
+          <div className="column">
+            <CodeSnippet language="html">
+              { htmlSelect }
+            </CodeSnippet>
+          </div>
+          <div className="column">
+            <CodeSnippet language="css">
+              { cssSelect }
+            </CodeSnippet>
+          </div>
         </div>
+        <div className="columns">
+          <div className="column">
+            <PreviewBox>
+              <div>
+                <div className="styledSelect" >
+                  <select className="styledSelect__select">
+                    <option>Berlin</option>
+                    <option>Mieten</option>
+                    <option>Preis ab 21939208109€</option>
+                    <option>ab 2 Zimmer</option>
+                  </select>
+                  <i className="styledSelect__icon" />
+                </div>
+              </div>
+              <div>
+                <div className="styledSelect styledSelect--secondary" >
+                  <select className="styledSelect__select">
+                    <option>Berlin</option>
+                    <option>Mieten</option>
+                    <option>Preis ab 21939208109€</option>
+                    <option>ab 2 Zimmer</option>
+                  </select>
+                  <i className="styledSelect__icon" />
+                </div>
+              </div>
+            </PreviewBox>
+          </div>
+        </div>
+
       </div>
     </div>
   </Page>);
--- src/components/App/FormPage/snippets/form.snippet.js
+++ src/components/App/FormPage/snippets/form.snippet.js
@@ -28,3 +28,117 @@ export const htmlRadio = `<div className="radio--styled">
     Frau
   </label>
 </div>`;
+
+export const cssRadio = `.radio--styled {
+  .radio__label--styled {
+    box-sizing: border-box;
+    cursor: pointer;
+    display: inline-block;
+    height: 30px;
+    line-height: 30px;
+    padding-left: 15px;
+    position: relative;
+
+    &:before {
+      border: 1px solid #dadada;
+      height: 30px;
+      left: 0;
+      top: 0;
+      width: 30px;
+    }
+
+    &:before,
+    &:after {
+      background-color: #fff;
+      border-radius: 50%;
+      box-sizing: border-box;
+      content: "";
+      display: inline-block;
+      margin-left: -20px;
+      position: absolute;
+    }
+
+    &:after {
+      content: "";
+      height: 14px;
+      left: 8px;
+      top: 8px;
+      width: 14px;
+    }
+  }
+
+  input[type=radio]:checked+.radio__label--styled::after {
+    background-color: #feb901;
+  }
+}`;
+
+export const htmlSelect = `<div className="styledSelect styledSelect--secondary" >
+  <select className="styledSelect__select">
+    <option>Berlin</option>
+    <option>Mieten</option>
+    <option>Preis ab 21939208109€</option>
+    <option>ab 2 Zimmer</option>
+  </select>
+  <i className="styledSelect__icon" />
+</div>`;
+
+export const cssSelect = `.styledSelect {
+  height: 40px;
+  overflow: hidden;
+  display: inline-block;
+  position: relative;
+  border: solid 1px #dadada;
+  background-color: #f6f6f6;
+
+  .styledSelect__icon {
+    background: url(/assets/arrowdown.svg) ;
+    background-position: center;
+    background-repeat: no-repeat;
+    display: block;
+    position: absolute;
+    width: 40px;
+    height: 40px;
+    top: 0px;
+    right: 3px;
+    z-index: 1;
+    border-left: solid 1px #dadada;
+  }
+
+  .styledSelect__select {
+     background: transparent;
+     border: 0;
+     box-sizing: border-box;
+     font-size: 15px;
+     height: 40px;
+     padding: 5px;
+     padding-left: 15px;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    position: relative;
+    z-index: 10;
+    padding-right: 50px;
+    &::-ms-expand {
+      display: none;
+    }
+  }
+
+  &--secondary {
+    @include respond-to("tablet") {
+      background-color: white;
+      border: 0;
+      border-bottom: solid 1px #dadada;
+      font-size: 12px;
+      height: 30px;
+      .styledSelect__select {
+        padding-left: 5px;
+        height: 30px;
+      }
+      .styledSelect__icon {
+        border-left: 0;
+        height: 30px;
+        width: 30px;
+      }
+    }
+  }
+}`;
